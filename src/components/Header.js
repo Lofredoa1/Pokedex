1c86ed8bed8e4861307cdb4961d8404d5fe331d6
Anthony Lofredo <anthony.lofredo@gmail.com>
2022-01-12 15:22:22 -0500

    css added to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import "../App.css";
 
 const Header = () => {
     return (
         <div className="header">
             <center>
                 <h1>Pokédex</h1>
+                <div>
+                    <div>My Team</div>
+                    <div>Home</div>
+                </div>
                 <h6>&copy; Anthony Lofredo 2022 </h6>
             </center>
         </div>
     )
-}
+};
 
 export default Header
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -5,20 +5,45 @@ const PokemonCard = (props) => {
 
     const { pokemonDetails} = props;
 
+    const typeColors = {
+        normal : "#A8A878",
+        fire : "#F08031",
+        water : "#6890F0",
+        grass : "#77C850",
+        electric : "#F8D030",
+        flying : "#A890F0",
+        poison : "#9E3E9E",
+        ice : "#98D8D8",
+        fighting : "#C03027",
+        bug : "#A8B820",
+        ground : "#E0C068",
+        psychic : "#F85888",
+        rock : "#B8A038",
+        dragon : "#7038F8",
+        fairy : "#F0B6BC",
+        ghost : "#705898",
+        steel : "#B8B8CF"
+    };
+
+    
+
     return ( 
-        <>
         <div className="allpokemons">
             {pokemonDetails.map((pokemon,index) => (
-                    <div className="pokemoncard">
-                        <h2>#{pokemon.id} - {pokemon.name}</h2>
-                        <img src={pokemon.sprites.front_default} alt="pokemon"/>
+                <div className="pokemoncard" style={{backgroundColor: typeColors[`${pokemon.types[0].type.name}`]}} >
+                    <h3 className="pokemonID"><span>#{pokemon.id}</span></h3>
+                    <img src={pokemon.sprites.front_default} alt="pokemon"/>
+                    <div className="info"></div>
+                    <h1>{pokemon.name}</h1>
+                    <div className="types">
+                        <h2>{pokemon.types[0].type.name}</h2>
+                        {pokemon.types[1] ? <h2 style={{backgroundColor: typeColors[`${pokemon.types[1].type.name}`]}}>{pokemon.types[1].type.name}</h2> : null}
                     </div>
-                ))
-            }
+                </div>
+            ))}
         </div>
-        </>
     )
-}
+};
 
 
 export default PokemonCard
